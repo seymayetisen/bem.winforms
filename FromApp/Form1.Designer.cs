f524ce016b6c3717ffda40b47697b2d6a28f5e0d
Orhan Aygün <orhan.aygun@outlook.com>
2017-12-04 13:16:49 +0300

    proje eklendi

---
+++ FromApp/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FromApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyAd = new System.Windows.Forms.Label();
+            this.lblKimlikNo = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtKimlikNo = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lstKisi = new System.Windows.Forms.ListBox();
+            this.txtUzmanlik = new System.Windows.Forms.TextBox();
+            this.lblUzmanlik = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(13, 31);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(20, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyAd
+            // 
+            this.lblSoyAd.AutoSize = true;
+            this.lblSoyAd.Location = new System.Drawing.Point(13, 62);
+            this.lblSoyAd.Name = "lblSoyAd";
+            this.lblSoyAd.Size = new System.Drawing.Size(37, 13);
+            this.lblSoyAd.TabIndex = 1;
+            this.lblSoyAd.Text = "Soyad";
+            // 
+            // lblKimlikNo
+            // 
+            this.lblKimlikNo.AutoSize = true;
+            this.lblKimlikNo.Location = new System.Drawing.Point(12, 93);
+            this.lblKimlikNo.Name = "lblKimlikNo";
+            this.lblKimlikNo.Size = new System.Drawing.Size(74, 13);
+            this.lblKimlikNo.TabIndex = 2;
+            this.lblKimlikNo.Text = "T.C. Kimlik No";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(112, 24);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(104, 20);
+            this.txtAd.TabIndex = 3;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(112, 55);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(104, 20);
+            this.txtSoyad.TabIndex = 4;
+            // 
+            // txtKimlikNo
+            // 
+            this.txtKimlikNo.Location = new System.Drawing.Point(112, 86);
+            this.txtKimlikNo.Name = "txtKimlikNo";
+            this.txtKimlikNo.Size = new System.Drawing.Size(104, 20);
+            this.txtKimlikNo.TabIndex = 5;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(112, 161);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(104, 23);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Kaydet";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lstKisi
+            // 
+            this.lstKisi.FormattingEnabled = true;
+            this.lstKisi.Location = new System.Drawing.Point(236, 23);
+            this.lstKisi.Name = "lstKisi";
+            this.lstKisi.Size = new System.Drawing.Size(260, 160);
+            this.lstKisi.TabIndex = 7;
+            this.lstKisi.SelectedIndexChanged += new System.EventHandler(this.lstKisi_SelectedIndexChanged);
+            // 
+            // txtUzmanlik
+            // 
+            this.txtUzmanlik.Location = new System.Drawing.Point(112, 122);
+            this.txtUzmanlik.Name = "txtUzmanlik";
+            this.txtUzmanlik.Size = new System.Drawing.Size(104, 20);
+            this.txtUzmanlik.TabIndex = 8;
+            // 
+            // lblUzmanlik
+            // 
+            this.lblUzmanlik.AutoSize = true;
+            this.lblUzmanlik.Location = new System.Drawing.Point(13, 129);
+            this.lblUzmanlik.Name = "lblUzmanlik";
+            this.lblUzmanlik.Size = new System.Drawing.Size(50, 13);
+            this.lblUzmanlik.TabIndex = 9;
+            this.lblUzmanlik.Text = "Uzmanlık";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 209);
+            this.Controls.Add(this.lblUzmanlik);
+            this.Controls.Add(this.txtUzmanlik);
+            this.Controls.Add(this.lstKisi);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtKimlikNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblKimlikNo);
+            this.Controls.Add(this.lblSoyAd);
+            this.Controls.Add(this.lblAd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyAd;
+        private System.Windows.Forms.Label lblKimlikNo;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtKimlikNo;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lstKisi;
+        private System.Windows.Forms.TextBox txtUzmanlik;
+        private System.Windows.Forms.Label lblUzmanlik;
+    }
+}
+
---
+++ FromApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FromApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isUpdate;
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            var kisi = (isUpdate) ? (Kisi)lstKisi.SelectedItem : new Kisi();
+
+            kisi.Ad = txtAd.Text;
+            kisi.Soyad = txtSoyad.Text;
+            kisi.TcKimlikNo = txtKimlikNo.Text;
+            kisi.Uzmanlik = txtUzmanlik.Text;
+
+            if (!isUpdate)
+            {
+                lstKisi.Items.Add(kisi);
+            }
+            else
+            {
+                lstKisi.Items[lstKisi.SelectedIndex] = kisi;
+            }
+
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            txtKimlikNo.Text = "";
+            txtUzmanlik.Text = "";
+
+            isUpdate = false;
+        }
+
+        private void lstKisi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstKisi.SelectedItem is null)
+            {
+                return;
+            }
+
+            Kisi gelenKisi = lstKisi.SelectedItem as Kisi;
+
+            txtAd.Text = gelenKisi.Ad;
+            txtSoyad.Text = gelenKisi.Soyad;
+            txtKimlikNo.Text = gelenKisi.TcKimlikNo;
+            txtUzmanlik.Text = gelenKisi.Uzmanlik;
+
+            isUpdate = true;
+        }
+    }
+}
